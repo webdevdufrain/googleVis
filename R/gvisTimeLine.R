6651e2066efceceaebf9800d9c11db569378593a
webdevdufrain <webdev@dufrain.co.uk>
2016-08-05 10:28:25 +0100

    update gvisTimeline
    Copied code from http://stackoverflow.com/questions/32059308/custom-tooltips-for-gvistimeline-in-the-r-package-googlevis to be able to use tooltips

--- R/gvisTimeLine.R
+++ R/gvisTimeLine.R
@@ -127,32 +127,32 @@
 #' )
 #' }
 
-gvisTimeline <- function(data, rowlabel="", barlabel="", start="", 
-                         end="", options=list(), chartid){
-
+gvisTimeline <- function(data, rowlabel="", barlabel="", tooltip="", start="", end="", options=list(), chartid) {
   my.type <- "Timeline"
   dataName <- deparse(substitute(data))
-
-  my.options <- list(gvis=modifyList(list(width=600, height=200),options), dataName=dataName,
-                     data=list(rowlabel=rowlabel, barlabel=barlabel, start=start, end=end,
-                       allowed=c("number", "string", "date", "datetime"))
-                     )
-
- 
-  checked.data <- gvisCheckTimelineData(data, rl=rowlabel, bl=barlabel,
-                                        start=start, end=end)
-  
-  output <- gvisChart(type=my.type, checked.data=checked.data, options=my.options,
-                      chartid=chartid, package="timeline") 
-  
+  my.options <- list(
+    gvis=modifyList(list(width=600, height=200),options),
+    dataName=dataName,
+    data=list(
+      rowlabel=rowlabel,
+      barlabel=barlabel,
+      tooltip=tooltip,
+      start=start,
+      end=end,
+      allowed=c("number", "string", "date", "datetime")
+    )
+  )
+  checked.data <- gvisCheckTimelineData(data, rl=rowlabel, bl=barlabel, tt=tooltip, start=start, end=end)
+  output <- gvisChart(type=my.type, checked.data=checked.data, options=my.options,chartid=chartid, package="timeline") 
   return(output)
 }
 
-gvisCheckTimelineData <- function(data, rl, bl, start, end){
-
-  if(any(c(rl, bl, start, end) %in% ""))
+gvisCheckTimelineData <- function(data, rl, bl, tt, start, end) {
+  if (any(c(rl, bl, tt, start, end) %in% "")) {
     return(data)
-  else  
-    return(data[, c(rl, bl, start, end)])
+  } else {
+    return(data[, c(rl, bl, tt, start, end)])
+  }
 }
+
 
